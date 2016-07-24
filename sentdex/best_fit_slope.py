0355e392bac7f99930e5b8fae90d0788547b4abe
Felix Perez <perezmarquezfelix@gmail.com>
2016-07-23 21:07:25 -0300

    squared error

--- sentdex/best_fit_slope.py
+++ sentdex/best_fit_slope.py
@@ -22,17 +22,41 @@ def best_fit_slope_and_intercept(xs, ys):
     """
     get_m = (((np.mean(xs) * np.mean(ys)) - np.mean(xs * ys)) /
              ((np.mean(xs) * np.mean(xs)) - np.mean(xs * xs)))
-    get_b = np.mean(ys) - get_m*np.mean(xs)
+    get_b = np.mean(ys) - get_m * np.mean(xs)
     return get_m, get_b
+
+
+def squared_error(y_orig, y_line):
+    """
+    Total sum of squared errors
+    :param y_orig: original y points
+    :param y_line: fitted y points
+    :return: int square error
+    """
+    return sum((y_line - y_orig) ** 2)
+
+
+def coefficient_of_determination(y_orig, y_line):
+    """
+    :param y_orig: original y points
+    :param y_line: fitted y points
+    :return: int coefficient of determination
+    """
+    y_mean_line = np.array([np.mean(y_orig) for _ in y_orig])
+    square_error_regression = squared_error(y_orig, y_line)
+    square_error_y_mean = squared_error(y_orig, y_mean_line)
+    return 1 - (square_error_regression / square_error_y_mean)
 
 
 m, b = best_fit_slope_and_intercept(x, y)
 
-
-regression_line = [(m * x1) + b for x1 in x]
+regression_line = np.array([(m * x1) + b for x1 in x])
 
 predict_x = 8
-predict_y = m*predict_x + b
+predict_y = m * predict_x + b
+
+r_squared = coefficient_of_determination(y, regression_line)
+print(r_squared)
 
 # Plot
 plt.scatter(x, y)
